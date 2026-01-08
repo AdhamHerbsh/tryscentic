c9f5460feec5100be79359153e875c9c074e4902
AdhamHerbsh <adhamabdelsamie05@gmail.com>
2026-01-08 19:30:45 +0200

    Production Demo V1.8

--- src/components/parts/SocialIcons.tsx
+++ src/components/parts/SocialIcons.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
 import Link from "next/link";
-import FacebookIcon from "@/assets/images/icons/facebook.svg"
-import InstagramIcon from "@/assets/images/icons/instagram.svg"
-import WhatsappIcon from "@/assets/images/icons/whatsapp-business.svg"
-import TiktokIcon from "@/assets/images/icons/tiktok.svg"
+import FacebookIcon from "/public/assets/images/icons/facebook.svg"
+import InstagramIcon from "/public/assets/images/icons/instagram.svg"
+import WhatsappIcon from "/public/assets/images/icons/whatsapp-business.svg"
+import TiktokIcon from "/public/assets/images/icons/tiktok.svg"
 
 
 export default function SocialIcons() {
@@ -11,37 +11,41 @@ export default function SocialIcons() {
         <div>
             <div className="max-w-6xl mx-auto px-6 text-white">
 
-                <div className="flex justify-center gap-4">
+                <div className="flex justify-center items-center gap-4">
                     <Link href="https://www.facebook.com/tryscentic/" target="_blank">
                         <Image
                             src={FacebookIcon}
                             alt="Facebook"
-                            fill
-                            className="object-cover footer-icons"
+                            width={32}
+                            height={32}
+                            className="footer-icons"
                         />
                     </Link>
                     <Link href="https://www.instagram.com/tryscentic/" target="_blank">
                         <Image
                             src={InstagramIcon}
                             alt="Instagram"
-                            fill
-                            className="object-cover footer-icons"
+                            width={32}
+                            height={32}
+                            className="footer-icons"
                         />
                     </Link>
                     <Link href="https://wa.me/+201090767839" target="_blank">
                         <Image
                             src={WhatsappIcon}
                             alt="Whatsapp"
-                            fill
-                            className="object-cover footer-icons"
+                            width={32}
+                            height={32}
+                            className="footer-icons"
                         />
                     </Link>
                     <Link href="https://www.tiktok.com/@tryscentic" target="_blank">
                         <Image
                             src={TiktokIcon}
                             alt="Tiktok"
-                            fill
-                            className="object-cover footer-icons"
+                            width={32}
+                            height={32}
+                            className="footer-icons"
                         />
                     </Link>
                 </div>
